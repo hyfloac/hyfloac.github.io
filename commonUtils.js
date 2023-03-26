ce1a7623311a3846e1999a684e72f43a12017c7f
hyfloac <hyfloac@gmail.com>
2023-03-26 17:04:16 -0500

    Moved some common functions to a common file

--- commonUtils.js
+++ commonUtils.js
@@ -9,3 +9,44 @@ function formatMoney(value)
 {
     return (value * 1.0).toLocaleString(undefined, { maximumFractionDigits: 0 })
 }
+
+function onMoneyFieldEnter(event)
+{
+    if(event.target.underlyingValue !== undefined)
+    {
+        event.target.value = event.target.underlyingValue;
+    }
+}
+
+function onMoneyFieldExit(event)
+{
+    const field = event.target;
+
+    field.underlyingValue = field.value * 1;
+    field.value = formatMoney(field.value * 1);
+}
+
+function registerMoneyFormatter(field)
+{
+    field.addEventListener("focus", onMoneyFieldEnter);
+    field.addEventListener("blur", onMoneyFieldExit);
+    field.addEventListener("input", () => {
+        field.underlyingValue = field.value;
+    });
+
+    field.value = field.value.replace(/[^0-9]/, "") * 1;
+    field.dispatchEvent(new Event("blur"));
+}
+
+function onRangeEvent(dest, src, callback) 
+{
+    dest.value = src.value;
+    dest.dispatchEvent(new Event("blur"));
+    callback();
+}
+
+function registerRangePair(input0, input1, callback) 
+{
+    input0.addEventListener("input", () => onRangeEvent(input1, input0, callback));
+    input1.addEventListener("input", () => onRangeEvent(input0, input1, callback));
+}
