554d1592e00fa40306b751bc372247222c52ea92
hyfloac <hyfloac@gmail.com>
2023-03-27 11:53:52 -0500

    Added separate taxes file

---
+++ taxes.js
@@ -0,0 +1,274 @@
+"use strict";
+
+function get2022SingleFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 10276
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 41776
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 89076
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 170051
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 215951
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 539901
+        }
+    ];
+}
+
+function get2022MarriedJointFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 20551
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 83551
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 178151
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 340101
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 431901
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 647851
+        }
+    ];
+}
+
+function get2022MarriedSeparateFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 10276
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 41776
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 89076
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 170051
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 215951
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 323926
+        }
+    ];
+}
+
+function get2022HeadFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 14651
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 55901
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 89051
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 170051
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 215951
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 539901
+        }
+    ];
+}
+
+function get2023SingleFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 11001
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 44726
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 95376
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 182101
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 231251
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 578126
+        }
+    ];
+}
+
+function get2023MarriedJointFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 22001
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 89451
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 190751
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 364201
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 462501
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 693751
+        }
+    ];
+}
+
+function get2023MarriedSeparateFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 11001
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 44726
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 95376
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 182101
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 231251
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 346876
+        }
+    ];
+}
+
+function get2023HeadFederalTaxes()
+{
+    return [
+        {
+            taxRate: 10,
+            taxableIncome: 0
+        },
+        {
+            taxRate: 12,
+            taxableIncome: 15701
+        },
+        {
+            taxRate: 22,
+            taxableIncome: 59851
+        },
+        {
+            taxRate: 24,
+            taxableIncome: 95351
+        },
+        {
+            taxRate: 32,
+            taxableIncome: 182101
+        },
+        {
+            taxRate: 35,
+            taxableIncome: 231251
+        },
+        {
+            taxRate: 37,
+            taxableIncome: 578101
+        }
+    ];
+}
+
